f9fb45273f57d77a5dd7700e51b04a90f785b3f3
meliu666 <meliu88@icloud.com>
2025-08-15 19:38:10 +0800

    Create 1005.swift

---
+++ 1005.swift
@@ -0,0 +1,80 @@
+
+import Foundation
+import SwiftData
+@Model
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+    
+    var isBirthdayToday:Bool{
+        Calendar.current.isDateInToday(birthday)
+    }
+}
+
+
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Query(sort: \Friend.birthday) private var friends:[Friend]
+    @Environment(\.modelContext) private var context
+    @State private var newName = ""
+    @State private var newDate = Date.now
+    
+    
+    var body: some View {
+        
+        NavigationStack {
+            List (friends){friend in
+                
+                HStack {
+                    if friend.isBirthdayToday{
+                        Image(systemName: "birthday.cake")
+                    }
+                    
+                    Text(friend.name)
+                        .bold(friend.isBirthdayToday)
+                    Spacer()
+                    Text(friend.birthday,format: .dateTime.month(.wide).day().year())
+                    
+                }
+                
+            }
+        }
+        .navigationTitle("Birthdays")
+        .safeAreaInset(edge: .bottom) {
+            VStack (alignment: .center,spacing: 20){
+                Text("New Birthday")
+                    .font(.headline)
+                DatePicker(selection: $newDate,in: Date.distantPast...Date.now,displayedComponents: .date) {
+                    TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Button("Save"){
+                    let newFriend = Friend(name: newName, birthday: newDate)
+                    context.insert(newFriend)
+                    
+                    newName = ""
+                    newDate = .now
+                }
+                .bold()
+            }
+            .padding()
+            .background(.bar)
+        }
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for:Friend.self,inMemory:true)
+    
+}
