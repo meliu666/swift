745748cbe127ff9752dadd140f8416e05231c172
meliu666 <215860445+meliu666@users.noreply.github.com>
2025-08-22 17:46:39 +0800

    Update 1001.swift

--- 1001.swift
+++ 1001.swift
@@ -1,56 +1,52 @@
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        HStack {
-            DayForecast(isRainy: true, high: 70, low:20)
-            
-            DayForecast(isRainy: false, high: 60)
+@main
+struct KKKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
     }
 }
 
-#Preview {
-    ContentView()
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            DayFor(day: "Monday", isRainy:true,high:30)
+            DayFor(day: "Tuesday", isRainy:false,high:30,low: 20)
+        }
+    }
 }
 
 
-struct DayForecast: View {
-    let day: String = "day"
-    let isRainy: Bool
-    let high: Int
-    var low: Int = 15
+struct DayFor : View {
+    let day : String
+    let isRainy : Bool
+    let high : Int
+    var low : Int = 15
     
-    var iconName: String {
+    var iconName : String {
         if isRainy {
             return "cloud.rain.fill"
         } else {
             return "sun.max.fill"
         }
     }
-    
-    var iconColor: Color {
-        if isRainy {
-            return Color.blue
-        } else {
-            return Color.yellow
-        }
-    }
-    
+  
     var body: some View {
         VStack {
             Text(day)
-                .font(Font.headline)
             Image(systemName: iconName)
-                .foregroundStyle(iconColor)
-                .font(Font.largeTitle)
-                .padding(5)
-            Text("High: \(high)")
-                .fontWeight(Font.Weight.semibold)
-            Text("Low: \(low)")
-                .fontWeight(Font.Weight.medium)
-                .foregroundStyle(Color.secondary)
+            Text("high:\(high)")
+            Text("low:\(low)")
         }
-        .padding()
     }
+}
+
+
+#Preview {
+    ContentView()
 }
