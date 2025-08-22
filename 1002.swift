434c3c9537d1af31e26eaedb57f538e2b93cedf9
meliu666 <215860445+meliu666@users.noreply.github.com>
2025-08-22 18:04:10 +0800

    Update 1002.swift

--- 1002.swift
+++ 1002.swift
@@ -1,78 +1,35 @@
-//
-//  DiceView.swift
-//  DiceRoller
-//
-//
-//
-
-
 import SwiftUI
 
+@main
+struct KKKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
 
-struct DiceView: View {
-    @State private var numberOfPips: Int = 1
+
+
+struct ContentView: View {
+    @State private var numberOfPips : Int = 5
     
     var body: some View {
+        
         VStack {
             Image(systemName: "die.face.\(numberOfPips).fill")
                 .resizable()
-                .frame(maxWidth: 100, maxHeight: 100)
-                .aspectRatio(1, contentMode: .fit)
-                .foregroundStyle(.black, .white)
+                .frame(width: 100,height: 100)
             
-            Button("Roll") {
-                withAnimation {
-                    numberOfPips = Int.random(in: 1...6)
-                }
+            Button("Rolls"){
+                numberOfPips = Int.random(in: 1...6)
             }
             .buttonStyle(.bordered)
         }
     }
 }
 
-
-struct ContentView: View {
-    @State private var numberOfDice: Int = 1
-    
-    var body: some View {
-        VStack {
-            Text("Dice Roller")
-                .font(.largeTitle.lowercaseSmallCaps())
-                .foregroundStyle(.white)
-            
-            HStack {
-                ForEach(1...numberOfDice, id: \.description) { _ in
-                    DiceView()
-                }
-            }
-            
-            HStack {
-                Button("Remove Dice", systemImage: "minus.circle.fill") {
-                    withAnimation {
-                        numberOfDice -= 1
-                    }
-                }
-                .disabled(numberOfDice == 1)
-                
-                Button("Add Dice", systemImage: "plus.circle.fill") {
-                    withAnimation {
-                        numberOfDice += 1
-                    }
-                }
-                .disabled(numberOfDice == 5)
-            }
-            .padding()
-            .labelStyle(.iconOnly)
-            .font(.title)
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.blue)
-        .tint(.white)
-    }
-}
-
-
-#Preview {
+#Preview(body: {
     ContentView()
-}
+})
+
